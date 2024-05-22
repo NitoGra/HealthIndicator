d97ae100600ae22095f851f44620ec52e884092f
ArkNito <b-teme@mail.ru>
2024-05-22 08:47:29 +0300

    Небольшие изменения

--- Assets/Scripts/TextHealthBar.cs
+++ Assets/Scripts/TextHealthBar.cs
@@ -6,5 +6,6 @@ public class TextHealthBar : HealthIndicator
 	[SerializeField] private TextMeshProUGUI _healthBar;
 
 	private void Start() => _healthBar.text = $"{Health.GetHealth}/{Health.GetMaxHealth}";
+
 	protected override void ChangeBar() => _healthBar.text = $"{Health.GetHealth}/{Health.GetMaxHealth}";
 }
